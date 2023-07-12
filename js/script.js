a1a431b81b2baf9888ca5a2bb0f09635c2f9b20e
mathrews <mateusps4gg@gmail.com>
2023-07-12 18:30:04 -0300

    adição de funções para aparecer as telas de adicionar participante e remover participante

--- js/script.js
+++ js/script.js
@@ -6,6 +6,8 @@ btnNovoTeam.onclick = function(){
 overlay.onclick = function(){
     overlay.classList.remove('active');
     formCriar.classList.remove('active');
+    deletarTeam.classList.remove('active');
+    addParticipante.classList.remove('active');
 }
 
 fecharFormCriar.onclick = function(){
@@ -13,29 +15,18 @@ fecharFormCriar.onclick = function(){
     formCriar.classList.remove('active');
 }
 
-const teams = [
-    
-];
-
-function listarTeams(){
-    listaDeTeams.innerHTML = '';
-    for(let i = 0; i < teams.length; i++){
-        listaDeTeams.innerHTML += `
-            <li>
-                <h5>${teams[i].nome} <box-icon name='show' type="solid"></box-icon></h5>
-                <h1> ${teams[i].participantes.length} <span>/${teams[i].qtd}</span></h1>
-                <div class="acoes">
-                    <button class="btn mini-title">adicionar</button>
-                    <button class="btn">
-                        <box-icon name='trash-alt' type="solid"></box-icon>
-                    </button>
-                </div>
-            </li>
-    `;
-    }
+fecharFormAdd.onclick = function(){
+    overlay.classList.remove('active');
+    addParticipante.classList.remove('active');
 }
 
-listarTeams();
+const teams = [
+    // {
+    //     nome: teamNome.value,
+    //     qtd: teamQtd.value,
+    //     participantes: [] 
+    // }
+];
 
 function adicionarTeam(){
     event.preventDefault();
@@ -51,3 +42,32 @@ function adicionarTeam(){
     listarTeams();
 }
 
+function listarTeams(){
+    listaDeTeams.innerHTML = '';
+    for(let i = 0; i < teams.length; i++){
+        listaDeTeams.innerHTML += `
+            <li>
+                <h5>${teams[i].nome} <box-icon name='show' type="solid"></box-icon></h5>
+                <h1> ${teams[i].participantes.length} <span>/${teams[i].qtd}</span></h1>
+                <div class="acoes">
+                    <button id="adicionar_participante" class="btn mini-title">adicionar</button>
+                    <button id="removerCard" class="btn">
+                        <box-icon name='trash-alt' type="solid"></box-icon>
+                    </button>
+                </div>
+            </li>
+    `;
+    }
+
+    let botaoRemoverCard = document.querySelector('#removerCard');
+
+    botaoRemoverCard.onclick = () => {
+        overlay.classList.add('active');
+        deletarTeam.classList.add('active');
+    }
+
+    adicionar_participante.onclick = () => {
+        overlay.classList.add('active');
+        addParticipante.classList.add('active');
+    }
+}
---
+++ removerTeam.md
@@ -0,0 +1,6 @@
+# Remover Team:
+
+1. Ao clicar, aparecer a aba de certeza da remoção
+
+2. Se sim: remover o team escolhido do array teams. Se não: Continuar com o estado atual.
+
