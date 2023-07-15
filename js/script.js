dc9095ffcaa43fa7434352a45312b75616cbbce4
mathrews <mateusps4gg@gmail.com>
2023-07-15 10:32:55 -0300

    deu ruim

--- js/script.js
+++ js/script.js
@@ -8,6 +8,7 @@ overlay.onclick = function(){
     formCriar.classList.remove('active');
     deletarTeam.classList.remove('active');
     addParticipante.classList.remove('active');
+    mostrarParticipantes.classList.remove('active');
 }
 
 fecharFormCriar.onclick = function(){
@@ -20,64 +21,20 @@ fecharFormAdd.onclick = function(){
     addParticipante.classList.remove('active');
 }
 
-const teams = [
-    // {
-    //     nome: teamNome.value,
-    //     qtd: teamQtd.value,
-    //     participantes: [] 
-    // }
+let teams = [
+    
 ];
-
-function adicionarTeam(){
-    event.preventDefault();
-    let team = {
-        nome: teamNome.value,
-        qtd: teamQtd.value,
-        participantes: [] 
-    }
-    teams.push(team);
-    fc.reset();
-    overlay.classList.remove('active');
-    formCriar.classList.remove('active');
-    listarTeams();
-}
-
-function gerarNovoTeams(){
-    for(let i = 0; i < teams.length && (i - teams.length) + 1; i++){
-        const teamsSemObjeto = [];
-    }
-}
-
-function removerTeamIndividual(){
-    removerTrue.onclick = () => {
-        overlay.classList.remove('active');
-        deletarTeam.classList.remove('active');
-        console.log('removido');
-        gerarNovoTeams();
-        listarTeams();
-    }
-
-    removerFalse.onclick = () => {
-        overlay.classList.remove('active');
-        deletarTeam.classList.remove('active');
-        console.log('Não Removido');
-        listarTeams();
-    }
-
-}
-
-removerTeamIndividual();
 
 function listarTeams(){
     listaDeTeams.innerHTML = '';
     for(let i = 0; i < teams.length; i++){
         listaDeTeams.innerHTML += `
             <li>
-                <h5>${teams[i].nome} <box-icon name='show' type="solid"></box-icon></h5>
+                <h5>${teams[i].nome} <box-icon onclick="listarParticipantes(${teams[i].id})" name='show' type="solid"></box-icon></h5>
                 <h1> ${teams[i].participantes.length} <span>/${teams[i].qtd}</span></h1>
                 <div class="acoes">
                     <button id="adicionar_participante" class="btn mini-title">adicionar</button>
-                    <button id="removerCard" class="btn">
+                    <button id="removerCard" dataid="${teams[i].id}" class="btn">
                         <box-icon name='trash-alt' type="solid"></box-icon>
                     </button>
                 </div>
@@ -95,5 +52,71 @@ function listarTeams(){
     adicionar_participante.onclick = () => {
         overlay.classList.add('active');
         addParticipante.classList.add('active');
+    }
+}
+
+function adicionarTeam(){
+    event.preventDefault();
+    let team = {
+        id: (teams.length + 1),
+        nome: teamNome.value,
+        qtd: teamQtd.value,
+        participantes: [] 
+    }
+    teams.push(team);
+    fc.reset();
+    overlay.classList.remove('active');
+    formCriar.classList.remove('active');
+    listarTeams();
+}
+
+function deletarTeamNoArray(id){
+    let aux = [];
+    for(let i = 0; i < teams.length; i++){
+        if(teams[i].id != id){
+            aux.push(teams[i])
+        }
+    }
+    teams = aux;
+    listarTeams();
+}
+
+function removerTeamIndividual(){
+    removerTrue.onclick = () => {
+        // overlay.classList.remove('active');
+        // deletarTeam.classList.remove('active');
+        // console.log('removido');
+        // deletarTeamNoArray(removerCard.dataId);
+        // listarTeams();
+        let id = document.querySelector('#removerCard')
+        console.log(id['dataid']);
+    }
+
+    removerFalse.onclick = () => {
+        overlay.classList.remove('active');
+        deletarTeam.classList.remove('active');
+        console.log('Não Removido');
+        listarTeams();
+    }
+}
+removerTeamIndividual();
+
+function listarParticipantes(id){
+    overlay.classList.add('active');
+    mostrarParticipantes.classList.add('active');
+    listaDeParticipantes.innerHTML = '';
+
+    let team;
+    for(let i = 0; i < teams.length; i++){
+        if(teams[i].id === id){
+            team = teams[i];
+        }
+    }
+    for(let i = 0; i < team.participantes.length; i++){
+        listaDeParticipantes.innerHTML += `
+        <li>
+            ${team.participantes[i]} <box-icon name="trash-alt" type="solid"></box-icon>
+        </li>
+        `;
     }
 }
